c4ca94c3d56bf942e68d7fafb4f77b7cdf975e1c
cjbarre <cjbarre@gmail.com>
2023-04-21 12:54:03 -0500

    Make state internal for any given task manager.

--- src/cjbarre/task_manager.clj
+++ src/cjbarre/task_manager.clj
@@ -3,15 +3,13 @@
             [clojure.core.async :refer [<! go]]
             [clojure.string :as s]))
 
-(def tasks (atom {}))
-
 (defprotocol TaskManager
   (create-task [this task-description] "Create a new task, break it into sub-tasks and return the initial task state")
   (generate-sub-tasks [this task] "Generate sub-tasks for the given task using GPT")
   (process-sub-tasks [this sub-tasks] "Process the given sub-tasks and return a list of results")
   (generate-sub-task-result [this sub-task] "Generate the result for the given sub-task using GPT"))
 
-(defrecord SimpleTaskManager [conversation-manager]
+(defrecord SimpleTaskManager [conversation-manager db]
   TaskManager
   (create-task [this task-description]
     (let [conversation-id (cm/create-conversation conversation-manager)
@@ -23,7 +21,7 @@
                           :task-description task-description
                           :sub-tasks sub-tasks
                           :completed []}]
-          (swap! tasks assoc task-id task-state)
+          (swap! db assoc task-id task-state)
           task-state))))
   (generate-sub-tasks [this task]
     (let [conversation-id (cm/create-conversation conversation-manager)
@@ -43,8 +41,9 @@
           message {:role "system" :content (str "Complete sub-task: " (:content sub-task))}]
       (go
         (let [gpt-response (<! (cm/update-conversation conversation-manager conversation-id message))]
-          (swap! tasks update-in [(:task-id sub-task) :completed] conj {:sub-task sub-task :result gpt-response})
+          (swap! db update-in [(:task-id sub-task) :completed] conj {:sub-task sub-task :result gpt-response})
           gpt-response)))))
 
 (defn create-task-manager [conversation-manager]
-  (->SimpleTaskManager conversation-manager))
+  (let [tasks (atom {})]
+    (->SimpleTaskManager conversation-manager tasks)))
